69eb514249589406d22fa64f16e259c1b2907ddb
JGuzman87 <jguzman913@gmail.com>
2026-01-06 21:17:29 -0500

    adjusted styling for form

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,7 +2,7 @@ import Form from "@/components/Form";
 
 const SignupPage = () => {
   return (
-    <div className="flex justify-center min-h-screen shadow-2xl items-center">
+    <div className="flex justify-center min-h-screen shadow-2xl md:items-center">
       <Form />
     </div>
   );
