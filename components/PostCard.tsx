0c09f0083c7ef20820dfa974315fc0071b41cc2c
JGuzman87 <jguzman913@gmail.com>
2026-02-13 22:08:45 -0500

    fixed deletion issues

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,4 +1,5 @@
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 interface Props {
   isPending: boolean;
@@ -7,6 +8,10 @@ interface Props {
 }
 
 const PostCard = ({ isPending, data, error }: Props) => {
+  const queryClient = useQueryClient();
+
+ 
+
   const handleDelete = async (id: string) => {
     try {
       const response = await fetch(`/api/blogpost/${id}`, {
@@ -19,6 +24,14 @@ const PostCard = ({ isPending, data, error }: Props) => {
       console.error("failed to delte post", error);
     }
   };
+
+     const mutation = useMutation({
+       mutationFn: handleDelete,
+       onSuccess: () => {
+         queryClient.invalidateQueries({ queryKey: ["post"] });
+       },
+     });
+
   if (error) return "An error has occured" + error.message;
   return (
     <motion.div
@@ -29,37 +42,43 @@ const PostCard = ({ isPending, data, error }: Props) => {
     >
       {isPending
         ? "Loading..."
-        : data.map(
-            (stored: {
-              _id: string;
-              title: string | number;
-              content: string;
-            }) => (
-              <motion.div
-                key={stored._id}
-                className="card-body bg-white/50 backdrop-blur-lg text-black rounded-2xl"
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{
-                  type: "spring",
-                  ease: "easeInOut",
-                  duration: 1,
-                }}
-              >
-                <h2 className="font-bold text-2xl border-b-2 border-gray-500/30 ">
-                  {stored.title}
-                </h2>
-                <p>{stored.content}</p>
-                <button
-                  type="button"
-                  className="self-end btn"
-                  onClick={() => handleDelete(stored._id)}
+        : (
+          <AnimatePresence mode="popLayout">
+            {data.map(
+              (stored: {
+                _id: string;
+                title: string | number;
+                content: string;
+              }) => (
+                <motion.div
+                  key={stored._id}
+                  className="card-body bg-white/50 backdrop-blur-lg text-black rounded-2xl"
+                  initial={{ opacity: 0, y: 100 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, x: 50 }}
+                  transition={{
+                    type: "spring",
+                    ease: "easeInOut",
+                    duration: 0.5,
+                  }}
+                  layout
                 >
-                  X
-                </button>
-              </motion.div>
-            ),
-          )}
+                  <h2 className="font-bold text-2xl border-b-2 border-gray-500/30 ">
+                    {stored.title}
+                  </h2>
+                  <p>{stored.content}</p>
+                  <button
+                    type="button"
+                    className="self-end"
+                    onClick={() => mutation.mutate(stored._id)}
+                  >
+                    X
+                  </button>
+                </motion.div>
+              )
+            )}
+          </AnimatePresence>
+        )}
     </motion.div>
   );
 };
