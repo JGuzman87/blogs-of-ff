bec3cb2dd848bb0108836371ec8df4fd72014f9b
JGuzman87 <jguzman913@gmail.com>
2026-02-13 22:12:45 -0500

    modified loading font

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -10,8 +10,6 @@ interface Props {
 const PostCard = ({ isPending, data, error }: Props) => {
   const queryClient = useQueryClient();
 
- 
-
   const handleDelete = async (id: string) => {
     try {
       const response = await fetch(`/api/blogpost/${id}`, {
@@ -25,12 +23,12 @@ const PostCard = ({ isPending, data, error }: Props) => {
     }
   };
 
-     const mutation = useMutation({
-       mutationFn: handleDelete,
-       onSuccess: () => {
-         queryClient.invalidateQueries({ queryKey: ["post"] });
-       },
-     });
+  const mutation = useMutation({
+    mutationFn: handleDelete,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["post"] });
+    },
+  });
 
   if (error) return "An error has occured" + error.message;
   return (
@@ -40,45 +38,45 @@ const PostCard = ({ isPending, data, error }: Props) => {
       animate={{ opacity: 1, y: 0 }}
       transition={{ type: "spring", ease: "easeInOut", duration: 1 }}
     >
-      {isPending
-        ? "Loading..."
-        : (
-          <AnimatePresence mode="popLayout">
-            {data.map(
-              (stored: {
-                _id: string;
-                title: string | number;
-                content: string;
-              }) => (
-                <motion.div
-                  key={stored._id}
-                  className="card-body bg-white/50 backdrop-blur-lg text-black rounded-2xl"
-                  initial={{ opacity: 0, y: 100 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, x: 50 }}
-                  transition={{
-                    type: "spring",
-                    ease: "easeInOut",
-                    duration: 0.5,
-                  }}
-                  layout
+      {isPending ? (
+        <p className="font-bold skeleton skeleton-text">Loading...</p>
+      ) : (
+        <AnimatePresence mode="popLayout">
+          {data.map(
+            (stored: {
+              _id: string;
+              title: string | number;
+              content: string;
+            }) => (
+              <motion.div
+                key={stored._id}
+                className="card-body bg-white/50 backdrop-blur-lg text-black rounded-2xl"
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, x: 50 }}
+                transition={{
+                  type: "spring",
+                  ease: "easeInOut",
+                  duration: 0.5,
+                }}
+                layout
+              >
+                <h2 className="font-bold text-2xl border-b-2 border-gray-500/30 ">
+                  {stored.title}
+                </h2>
+                <p>{stored.content}</p>
+                <button
+                  type="button"
+                  className="self-end"
+                  onClick={() => mutation.mutate(stored._id)}
                 >
-                  <h2 className="font-bold text-2xl border-b-2 border-gray-500/30 ">
-                    {stored.title}
-                  </h2>
-                  <p>{stored.content}</p>
-                  <button
-                    type="button"
-                    className="self-end"
-                    onClick={() => mutation.mutate(stored._id)}
-                  >
-                    X
-                  </button>
-                </motion.div>
-              )
-            )}
-          </AnimatePresence>
-        )}
+                  X
+                </button>
+              </motion.div>
+            ),
+          )}
+        </AnimatePresence>
+      )}
     </motion.div>
   );
 };
